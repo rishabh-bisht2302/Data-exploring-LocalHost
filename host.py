c79c603c32dd0040f4ba8f7698d3e6583091beff
Rishabh Bisht <Rishabh.bisht2302@gmail.com>
2019-06-25 01:00:09 +0530

    feat: Add route to display all data

--- host.py
+++ host.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 def home():
     return "WELCOME TO BOOKS EXPLORING WORLD"
 
+@app.route('/v1/resource/all',methods=['GET'])
+def all():
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    result = cur.execute('SELECT * FROM books;').fetchall()
+    res_dict = dict_factory(result)
+    return jsonify(res_dict)
