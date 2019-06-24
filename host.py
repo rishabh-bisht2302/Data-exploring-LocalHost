aa9d6d734b5f8473c7453fcdbf11400e170813c5
Rishabh Bisht <Rishabh.bisht2302@gmail.com>
2019-06-25 01:04:02 +0530

    "feat: Function to convert list to dict"
    This function takes the list of tuple and convert the data to dictionary
    to facilitate ease of reading result

--- host.py
+++ host.py
@@ -13,3 +13,15 @@ def all():
     result = cur.execute('SELECT * FROM books;').fetchall()
     res_dict = dict_factory(result)
     return jsonify(res_dict)
+
+def dict_factory(list_of_tuple):
+    lst= []
+    for book in list_of_tuple:
+        d = {}
+        d['id'] = row[0]
+        d['published'] = row[1]
+        d['author'] = row[2]
+        d['title'] = row[3]
+        d['first_sentence'] = row[4]
+        l.append(d)
+    return l
